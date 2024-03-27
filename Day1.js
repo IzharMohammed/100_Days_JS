b30dc160f51b2599a41649b599c2667425e5b7d0
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-27 23:41:33 +0530

    Added thapa solution

--- Day1.js
+++ Day1.js
@@ -16,15 +16,20 @@
 //? If the input string is empty or contains only whitespace, the function should return an false.
 //? The function should ignore leading and trailing whitespace when determining the longest word.
 
-
+//Method-1(Me)
 function findLongestWord(str) {
-    // console.log(Math.max('Watch', 'Thapa' ));
+
     let arr = str.split(" ");
+
     console.log(arr);
+
     let ans = arr.reduce((first, second) => {
+
         console.log([first, second]);
+
         return first.length >= second.length ? first : second
     })
+
     return ans;
 }
 
@@ -32,3 +37,28 @@ function findLongestWord(str) {
 console.log(
   "Largest word is : ",  findLongestWord("How much wood would a woodchuck chuck if a woodchuck could chuck wood")
 );
+
+
+//Method-2(Thapa)
+const findLongestWord = (str) => {
+    if (str.trim().length === 0) {
+        return false;
+    }
+
+    let words = str.split(" ");
+
+    /*
+    Words.sort()-->It will sort elements according to unicode values 
+    and by inside by using callback fn it will sort according to descending order
+    */
+
+    words = words.sort((a, b) => b.length - a.length);
+
+    console.log(words);
+
+    return words[0];
+}
+
+console.log(findLongestWord(
+    "Watch Thapa Technical javascript awesomethapatechnical course on youtube"
+));

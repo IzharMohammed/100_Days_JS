a61ffb9e4f3e5e89bc4c7eb24d59b6dd6a71612e
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-29 15:44:00 +0530

    Added my day2 solution

---
+++ Day2.js
@@ -0,0 +1,36 @@
+// Programming Question: Hash Tag Generator
+
+// You are required to implement a function generateHash that generates a hash tag from a given input string.
+// The hash tag should be constructed as follows:
+
+// The input string should be converted to a hash tag format, where each word is capitalized and
+// concatenated together without spaces.
+
+// If the length of the input string is greater than 2 and the input string is empty or contains only whitespace,
+// the function should return false.
+// Otherwise, the function should return the generated hash prefixed with #.
+
+// Write a function generateHash to accomplish this task.
+
+function generatehash(str) {
+    //See if there is empty string
+    if(str.length === 0){
+        return false;
+    }
+    //convert to array of strings
+    let arr = str.split(" ");
+    console.log(arr);
+    //iterate through each element and make first character to upper and add
+    //remaining characters using slice 
+    const newArr = arr.map((elements) => {
+        return elements.slice(0, 1).toUpperCase() + elements.slice(1)
+    })
+
+    console.log(newArr);
+    const result = newArr.join('');
+    return '#' + result
+}
+
+console.log(generatehash('my name is mohammed izhar'));
+
+// o/p ==> "#MyNameIsMohammedIzhar"

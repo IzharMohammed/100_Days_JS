0b051247ea6332780512287d7cf6745b19dece15
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-29 16:18:15 +0530

    Added thapa solution of day 2

--- Day2.js
+++ Day2.js
@@ -12,14 +12,18 @@
 
 // Write a function generateHash to accomplish this task.
 
+// My solution
 function generatehash(str) {
+
     //See if there is empty string
-    if(str.length === 0){
+    if (str.length === 0) {
         return false;
     }
+
     //convert to array of strings
     let arr = str.split(" ");
     console.log(arr);
+
     //iterate through each element and make first character to upper and add
     //remaining characters using slice 
     const newArr = arr.map((elements) => {
@@ -27,10 +31,31 @@ function generatehash(str) {
     })
 
     console.log(newArr);
+
     const result = newArr.join('');
+
     return '#' + result
 }
 
 console.log(generatehash('my name is mohammed izhar'));
 
 // o/p ==> "#MyNameIsMohammedIzhar"
+
+
+//Thapa solution
+const generateHash = (str) => {
+    if (str.length > 280 || str.trim().length === 0) {
+        return false;
+    }
+
+    str = str.split(" ");
+    str = str.map((curElem) =>
+        curElem.replace(curElem[0], curElem[0].toUpperCase())
+    );
+
+    str = "#" + str.join("");
+    console.log(str);
+    return str;
+}
+
+console.log(generateHash("my name is thapa technical"));
